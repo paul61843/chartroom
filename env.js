5be8689312d4c7e61d9750a8ce05f16367176dc9
paul61843@gmail.com <paul61843@gmail.com>
2022-08-12 13:43:04 +0000

    base webSocket init

---
+++ env.js
@@ -0,0 +1,5 @@
+const PORT = 5002;
+
+export {
+    PORT,
+}
---
+++ node.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+const app = express();
+const PORT = 5500;
+
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+})
+
+const socket = new SocketServer({ server })
+
+socket.on('connection', ws => {
+    console.log('Client connected');
+
+    ws.on('message', data => {
+        data = data.toString();
+        
+        let clients = socket.clients;
+        clients.forEach(client => {
+            client.send(data);
+        })
+    })
+
+    ws.on('close', () => {
+        console.log('close connected');
+    })
+})
